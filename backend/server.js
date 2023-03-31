1dc14b01219244402fe100c084c15335e4a96233
xoocode <peter@xoocode.com>
2023-03-31 22:15:30 +0200

    Set up React frontend and update Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const session = require('express-session')
+const path = require('path')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 
@@ -17,5 +18,19 @@ app.use(
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res => {  res.send('Hello World!')}))
+
+// Serve frontend static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('Please set to production ...')
+  })
+}
 
 const server = app.listen(port, () => console.log(`Server started on port ${port} ... `))
